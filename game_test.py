a533b11e28622bd856cc6ec8488fa88fca864424
Sergei <sergeiyakima1@gmail.com>
2024-04-19 17:05:26 +0300

    updated to use the correct drawing method

--- game_test.py
+++ game_test.py
@@ -1,30 +1,7 @@
 from game import Game, Direction
 import pygame
 import constants
-from grid_window import draw_grid
-
-def draw_popup(FONT):
-    # setup constants
-    BLACK = (0, 0, 0)
-    LIGHT_RED = (255, 70, 70, 200)
-    w, h = 275, 100
-    x, y = (constants.WINDOW_SIZE - w)//2, (constants.WINDOW_SIZE - h)//3
-
-    # create a surface for the popup
-    popup_surface = pygame.Surface((w, h), pygame.SRCALPHA)
-    
-    # draw the background rectangle and border
-    pygame.draw.rect(popup_surface, LIGHT_RED, (0, 0, w, h))
-    pygame.draw.rect(popup_surface, BLACK, (0, 0, w, h), 1)
-    
-    # render the text
-    text_surface = FONT.render('Game Over!', True, BLACK)
-    # calculate text position
-    text_rect = text_surface.get_rect(center=(w//2, h//2))
-    # draw the text and textbox
-    popup_surface.blit(text_surface, text_rect)
-
-    return popup_surface, x, y
+from grid_window import draw_grid, draw_popup
 
 
 def main():
